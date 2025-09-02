bbe65dff12bd7dbd541b22cd9660bedd5f8b805b
ariacreation2-byte <ariacreation2@gmail.com>
2025-09-02 22:10:18 +0530

    Create ShippingBar.js

---
+++ components/ShippingBar.js
@@ -0,0 +1,8 @@
+// components/ShippingBar.js
+export default function ShippingBar(){
+  return (
+    <div className="ship">
+      International shipping available — India 🇮🇳 | Middle East 🇦🇪 | USA 🇺🇸 | Thailand 🇹🇭
+    </div>
+  );
+}

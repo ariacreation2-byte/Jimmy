549e13b200cfb150cd7c1d2c09dbea0cace60afb
ariacreation2-byte <ariacreation2@gmail.com>
2025-09-02 20:26:54 +0530

    Update and rename header.js to Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-export default function header() {
+export default function Header() {
   return (
     <header className="p-4 bg-blue-600 text-white text-center">
       <h1 className="text-2xl font-bold">Jimmy Junior Kids Wear</h1>

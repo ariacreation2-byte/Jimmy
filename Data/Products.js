427c50d07cf38fa394287ee9ae0d7df6a669cfc5
ariacreation2-byte <ariacreation2@gmail.com>
2025-09-03 01:34:21 +0530

    Create Products.js

---
+++ Data/Products.js
@@ -0,0 +1,39 @@
+const products = [
+  {
+    id: 1,
+    title: "Blazer 0x4",
+    size: "0x4",
+    price: "450",
+    image: "/product/5516.jpg",
+  },
+  {
+    id: 2,
+    title: "Printed Polo",
+    size: "16x26",
+    price: "899",
+    image: "https://images.unsplash.com/photo-1539533113208-f6df8cc8b543?q=80&w=1200",
+  },
+  {
+    id: 3,
+    title: "Casual Shorts",
+    size: "10x20",
+    price: "799",
+    image: "https://images.unsplash.com/photo-1618354694520-3f9b8b87b84b?q=80&w=1200",
+  },
+  {
+    id: 4,
+    title: "Denim Shirt",
+    size: "12x22",
+    price: "999",
+    image: "https://images.unsplash.com/photo-1503342217505-b0a15cf70489?q=80&w=1200",
+  },
+  {
+    id: 5,
+    title: "Classic Hoodie",
+    size: "16x26",
+    price: "1200",
+    image: "https://images.unsplash.com/photo-1602810318383-e3b95a03b7b2?q=80&w=1200",
+  },
+];
+
+export default products;

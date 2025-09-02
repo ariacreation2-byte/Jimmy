13cc6e7e4fdef44c3e37d64c8a541246ff477c12
ariacreation2-byte <ariacreation2@gmail.com>
2025-09-02 20:25:45 +0530

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,4 +1,4 @@
-export default function Header() {
+export default function header() {
   return (
     <header className="p-4 bg-blue-600 text-white text-center">
       <h1 className="text-2xl font-bold">Jimmy Junior Kids Wear</h1>

4ce4cf1570c12ad01f4124767181363a9c7c155d
ariacreation2-byte <ariacreation2@gmail.com>
2025-09-02 20:04:40 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>🚀 Hello Jimmy Junior</h1>
-      <p>Basic Website Working ✅</p>
-    </div>
-  );
-}
+import Header from "../components/Header";
